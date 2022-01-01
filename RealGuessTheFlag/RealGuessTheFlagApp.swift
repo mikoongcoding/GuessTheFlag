968a5fd3960ed484f65224313e064e84e8d8127b
mikoongcoding <skymikong@gmail.com>
2022-01-01 22:31:04 +0900

    Initial Commit

---
+++ RealGuessTheFlag/RealGuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealGuessTheFlagApp.swift
+//  RealGuessTheFlag
+//
+//  Created by Mi Gyeong Park on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct RealGuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
